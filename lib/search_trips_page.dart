20eab2d2025468af904241baaec633d2e45b3852
CharlesSinchigal <csinchigalo0552@uta.edu.ec>
2024-05-01 20:29:34 -0500

    Intento del Buscador

--- lib/search_trips_page.dart
+++ lib/search_trips_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
 
 class SearchTripsPage extends StatelessWidget {
-  const SearchTripsPage({super.key});
+  const SearchTripsPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SEARCH'), // Texto: SEARCH
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color(0xFF4268D3),
+              const Color(0xFF1976D2)
+            ], // Fondo degradado
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          // Centrar contenido verticalmente
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  textAlign:
+                      TextAlign.center, // Centrar el texto del campo de entrada
+                  decoration: InputDecoration(
+                    hintText:
+                        'Sinchigalo Charles', // Control para ingreso de datos
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción al presionar el botón de búsqueda
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Búsqueda realizada Charles')),
+                    );
+                  },
+                  child: const Text('Buscar'), // Botón de búsqueda
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
